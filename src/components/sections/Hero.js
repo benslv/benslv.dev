984ee09ba8b4980fd49c20c1db470aee21ec7452
Ben Silverman <bensilverman10@gmail.com>
2021-02-14 22:30:37 +0000

    Finish Hero component

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -2,17 +2,17 @@ import React from "react";
 import { Chat } from "phosphor-react";
 
 import Button from "../Button";
-
+import Section from "../Section";
 
 const HeroSection = () => (
-	<React.Fragment>
+	<Section>
 		<h1>Hi, I'm Ben!</h1>
-		<p>
+		<p style={{ fontSize: "18px" }}>
 			I'm a third-year Computer Science student at the University of York,
 			currently on placement at Arm.
 		</p>
-    <Button icon={<Chat/>} text="Get in touch!" />
-	</React.Fragment>
+		<Button icon={<Chat weight="bold" />} text="Get in touch!" />
+	</Section>
 );
 
 export default HeroSection;

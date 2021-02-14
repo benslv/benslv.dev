cf0da7b8051b26499e40efda19c63b795e660f9f
Ben Silverman <bensilverman10@gmail.com>
2021-02-14 15:38:58 +0000

    Move GlobalStyle to its own file

---
+++ src/GlobalStyle.js
@@ -0,0 +1,47 @@
+import { createGlobalStyle } from "styled-components";
+
+import theme from "./theme";
+const { colors, fonts } = theme;
+
+export default createGlobalStyle`
+  html {
+    box-sizing: border-box;
+    width: 100;
+  }
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+
+    background-color: ${colors.background};
+    color: ${colors.text.body};
+    font-family: "Inter", sans-serif;
+    font-weight: 400;
+    font-size: ${fonts.sizes.regular};
+  }
+
+  h1, h2, h3, p {
+    margin: 0;
+    padding: 0;
+  }
+
+  h1, h2, h3 {
+    color: ${colors.text.header};
+    margin-bottom: 0.25em;
+    font-size: ${fonts.sizes.large};
+  }
+
+  h1 {
+    font-size: ${fonts.sizes.xLarge}
+  }
+
+  p { 
+    margin-bottom: 1em;
+  }
+`;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,26 +1,14 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-import styled, { createGlobalStyle } from "styled-components";
 import "fontsource-inter/400.css";
 import "fontsource-inter/700.css";
 
+// Import components
 import Navbar from "./Navbar";
 import Container from "./Container";
-
-import theme from "../theme";
 import Footer from "./Footer";
-const { colors } = theme;
 
-const GlobalStyle = createGlobalStyle`
-	body {
-		background-color: ${colors.background};
-		color: ${colors.text.body};
-		margin: 0;
-		padding: 0;
-		font-family: "Inter", sans-serif;
-		font-weight: 400;
-	}
-`;
+import GlobalStyle from "../GlobalStyle";
 
 const Layout = ({ title, children }) => {
 	return (

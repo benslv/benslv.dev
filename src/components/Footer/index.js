ebcd96174dcf21e05fb25d9f7062986ef6a83dc8
Ben Silverman <bensilverman10@gmail.com>
2021-05-03 19:54:05 +0100

    Add footer content

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,14 +8,24 @@ const Wrapper = styled.footer`
   justify-content: center;
   align-items: center;
 
+  margin-top: var(--font-size-lg);
   padding: var(--font-size-lg) 0;
+
+  p {
+    color: var(--color-accent);
+    font-weight: 500;
+    background-color: var(--color-accent-20);
+    padding: calc(0.5 * var(--font-size-base)) var(--font-size-base);
+    border-radius: var(--border-radius);
+    box-shadow: var(--box-shadow-light);
+  }
 `;
 
 export const Footer = () => {
   return (
     <Wrapper>
       <p>
-        Made with <Emoji emoji="☕"/> in the UK!
+        Made with <Emoji emoji="☕" /> in the UK!
       </p>
     </Wrapper>
   );

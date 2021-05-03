4705ddf94fc989c774a817b53406a2dc8b3452d9
Ben Silverman <bensilverman10@gmail.com>
2021-05-03 18:31:13 +0100

    Update Layout with navbar and footer

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 
+import { Navbar } from "../Navbar";
 import { Container } from "../Container";
+import { Footer } from "../Footer";
 
 import { GlobalStyle } from "../../theme/GlobalStyle";
 
@@ -8,7 +10,9 @@ export const Layout = ({ children }) => {
   return (
     <>
       <GlobalStyle />
+      <Navbar />
       <Container>{children}</Container>
+      <Footer />
     </>
   );
 };

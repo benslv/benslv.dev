7fbf3988d0d3e5c5d7e78cec8acb4bd0860cf5a3
Ben Silverman <bensilverman10@gmail.com>
2021-05-03 18:31:34 +0100

    Add navbar entries to content

---
+++ src/content/config.js
@@ -0,0 +1,27 @@
+export const skills = [
+  "JavaScript (ES6)",
+  "React",
+  "Gatsby",
+  "Express",
+  "Python",
+  "Axios",
+  "TinyHTTP",
+  "Figma",
+  "Photoshop",
+  "Needle",
+  "Svelte",
+  "HTML",
+  "CSS",
+  "Docker",
+];
+
+export const nav = [
+  {
+    name: "Home",
+    to: "/",
+  },
+  {
+    name: "Blog",
+    to: "/blog",
+  },
+];

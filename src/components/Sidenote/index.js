1b8c5042d2d0db2baddf1615be1ad599d7b5360d
Ben Silverman <bensilverman10@gmail.com>
2021-05-04 11:36:49 +0100

    Adjust Sidenote styling for accessibility

--- src/components/Sidenote/index.js
+++ src/components/Sidenote/index.js
@@ -5,23 +5,34 @@ export const Sidenote = styled.aside`
   margin-bottom: var(--font-size-base);
   border-radius: var(--border-radius);
 
-  color: var(--color-text);
+  color: var(--color-heading);
   background-color: var(--color-card-background);
   border: 1px solid var(--color-card-border);
 
   &.info {
-    color: var(--color-accent);
     background-color: var(--color-accent-20);
-    border: 1px solid var(--color-accent);
+   
+    border: none;
+    border-left: 5px solid var(--color-accent);
 
     box-shadow: 0 0 15px var(--color-accent-20);
   }
 
   &.warn {
-    color: var(--color-warn);
     background-color: var(--color-warn-20);
-    border: 1px solid var(--color-warn);
+
+    border: 0;
+    border-left: 5px solid var(--color-warn);
 
     box-shadow: 0 0 15px var(--color-warn-20);
+  }
+
+  &.error {
+    background-color: var(--color-error-20);
+
+    border: 0;
+    border-left: 5px solid var(--color-error);
+
+    box-shadow: 0 0 15px var(--color-error-20);
   }
 `;
--- src/components/TextLink/index.js
+++ src/components/TextLink/index.js
@@ -17,13 +17,33 @@ const StyledLink = styled(Link)`
     border-bottom: 2px solid var(--color-heading);
   }
 
-  ${Sidenote}.warn > & {
-    color: var(--color-warn);
-    border-bottom: 2px solid var(--color-warn-20);
+  ${Sidenote} & {
+    color: var(--color-heading);
+    border-bottom: 2px solid var(--color-heading);
 
     &:hover {
-      color: var(--color-heading);
-      border-bottom: 2px solid var(--color-heading);
+      color: var(--color-accent);
+      border-bottom: 2px solid var(--color-accent-20);
+    }
+  }
+
+  ${Sidenote}.warn & {
+    color: var(--color-heading);
+    border-bottom: 2px solid var(--color-heading);
+
+    &:hover {
+      color: var(--color-warn);
+      border-bottom: 2px solid var(--color-warn-20);
+    }
+  }
+
+  ${Sidenote}.error & {
+    color: var(--color-heading);
+    border-bottom: 2px solid var(--color-heading);
+
+    &:hover {
+      color: var(--color-error);
+      border-bottom: 2px solid var(--color-error-20);
     }
   }
 `;

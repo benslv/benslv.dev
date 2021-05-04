0ab578cac8f0e5b6a0298ee521cc989778fec4c4
Ben Silverman <bensilverman10@gmail.com>
2021-05-04 10:54:23 +0100

    Add SkipToContent button

---
+++ src/components/SkipToContent/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+import { blueBox } from "../../theme/mixins";
+
+const SkipLink = styled.a`
+  position: fixed;
+  left: 50px;
+  top: 100px;
+  transform: translateX(-150%);
+  transition: transform 0.175s var(--easing);
+
+  ${blueBox};
+
+  &:focus {
+    transform: translateX(50px);
+  }
+`;
+
+export const SkipToContent = () => {
+  return (
+    <SkipLink href="#main-content">
+      Skip to content
+    </SkipLink>
+  );
+};

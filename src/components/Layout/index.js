61f259ef5ab6eb62dcbc2a0e260afd1e1dff0620
Ben Silverman <bensilverman10@gmail.com>
2021-05-05 21:07:16 +0100

    Add basic React Helmet SEO

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,14 +3,16 @@ import React from "react";
 import { Navbar } from "../Navbar";
 import { Container } from "../Container";
 import { Footer } from "../Footer";
+import { SkipToContent } from "../SkipToContent";
+import { SEO } from "../SEO";
 
 import { GlobalStyle } from "../../theme/GlobalStyle";
-import { SkipToContent } from "../SkipToContent";
 
-export const Layout = ({ children }) => {
+export const Layout = ({ title, description, children }) => {
   return (
     <>
       <GlobalStyle />
+      <SEO title={title} description={description} />
       <SkipToContent />
       <Navbar />
       <Container id="main-content">{children}</Container>

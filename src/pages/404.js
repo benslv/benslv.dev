22e8301c051df96122828a0b8ef888f0df247f08
Ben Silverman <bensilverman10@gmail.com>
2021-06-02 20:24:42 +0100

    Add a 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Container } from "../components/Container";
+import { Emoji } from "../components/Emoji";
+import { Layout } from "../components/Layout";
+
+const NotFoundPage = () => {
+  return (
+    <Layout>
+      <Container id="main-content">
+        <section>
+          <h2>Oops!</h2>
+          <p>
+            It doesn't look like that pages exists <Emoji emoji="😕" />
+          </p>
+          <p>Maybe double check the URL to make sure it's correct?</p>
+        </section>
+      </Container>
+    </Layout>
+  );
+};
+
+export default NotFoundPage;

4bee940012b67d991a1ab2dbcc9aef8da147458a
Ben Silverman <bensilverman10@gmail.com>
2021-07-27 13:50:24 +0100

    Install ESlint TS packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,15 @@ module.exports = {
   globals: {
     __PATH_PREFIX__: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
+  extends: [
+    "next",
+    "next/core-web-vitals",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -15,7 +23,7 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: "module",
   },
-  plugins: ["react"],
+  plugins: ["react", "@typescript-eslint"],
   rules: {
     indent: ["error", 2],
     quotes: ["error", "double"],

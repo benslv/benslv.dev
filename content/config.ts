dc5baf88fd03360e18483d1cb05b223560f0400d
Ben Silverman <bensilverman10@gmail.com>
2021-08-08 12:28:44 +0100

    Add config.ts

---
+++ content/config.ts
@@ -0,0 +1,35 @@
+export const skills = [
+  "JavaScript (ES6)",
+  "React",
+  "Gatsby",
+  "Express",
+  "Python",
+  "Axios",
+  "TinyHTTP",
+  "Figma",
+  "Photoshop",
+  "Needle",
+  "Svelte",
+  "HTML",
+  "CSS",
+  "Docker",
+];
+
+export const nav = [
+  {
+    name: "Home",
+    to: "/",
+  },
+  {
+    name: "About",
+    to: "/about",
+  },
+  {
+    name: "Projects",
+    to: "/projects",
+  },
+  {
+    name: "Posts",
+    to: "/posts",
+  },
+];

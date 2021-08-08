82f68d2a2393830e53396dea4c6852cf42262272
Ben Silverman <bensilverman10@gmail.com>
2021-08-08 12:54:11 +0100

    Add navbar component

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,5 +1,48 @@
 import React from "react";
+import { styled } from "../../stitches.config";
 
-export const index = () => {
-  return <div></div>;
-};
+import { Emoji } from "../Emoji";
+import { UnstyledLink } from "../UnstyledLink";
+
+import { nav } from "../../content/config";
+
+const Wrapper = styled("div", {
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+
+  padding: "$2 calc(1.5* $2)",
+
+  backgroundColor: "$background",
+});
+
+const Nav = styled("nav", {
+  display: "flex",
+  alignItems: "center",
+  gap: "$2",
+});
+
+const NavLink = styled(UnstyledLink, {
+  color: "$text",
+  fontWeight: 500,
+
+  transition: "color 0.175s $ease",
+
+  "&:hover": {
+    color: "$accent",
+  },
+});
+
+export const Navbar = (): JSX.Element => (
+  <Wrapper>
+    <Emoji emoji="👋" size="48px" />
+    <Nav>
+      {nav &&
+        nav.map(({ name, to }, i) => (
+          <NavLink to={to} key={i}>
+            {name}
+          </NavLink>
+        ))}
+    </Nav>
+  </Wrapper>
+);

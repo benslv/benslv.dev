b0086d98fa3cf2c8e68f496a07816f4a3f68144a
Ben Silverman <bensilverman10@gmail.com>
2021-09-11 15:49:25 +0100

    Fix date typing

--- lib/posts.ts
+++ lib/posts.ts
@@ -5,7 +5,7 @@ import { serialize } from "next-mdx-remote/serialize";
 
 const postsDirectory = path.join(process.cwd(), "content/posts");
 
-export const getSortedPostsData = (): { slug: string }[] => {
+export const getSortedPostsData = (): { slug: string; date: string }[] => {
   // Get file names under /posts
   const fileNames = fs.readdirSync(postsDirectory);
   const allPostsData = fileNames.map((fileName) => {
@@ -17,14 +17,18 @@ export const getSortedPostsData = (): { slug: string }[] => {
     const fileContents = fs.readFileSync(fullPath, "utf8");
 
     // Use gray-matter to parse the post metadata section
-    const matterResult = matter(fileContents);
+    const { data } = matter(fileContents);
+
+    console.log(data.date);
 
     // Combine the data with the id
     return {
       slug,
-      ...matterResult.data,
+      date: data.date,
+      ...data,
     };
   });
+
   // Sort posts by date
   return allPostsData.sort(({ date: a }, { date: b }) => {
     if (a < b) {
@@ -53,7 +57,9 @@ export const getAllPostIDs = (): {
   });
 };
 
-export const getPostData = async (slug: string): Promise<{ slug: string; source: any }> => {
+type Slug = string | string[] | undefined;
+
+export const getPostData = async (slug: Slug): Promise<{ slug: Slug; source: any }> => {
   const fullPath = path.join(postsDirectory, `${slug}.mdx`);
   const fileContents = fs.readFileSync(fullPath, "utf8");
 

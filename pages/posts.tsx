1febe1f7736e06ea057c75d2c6e98ac663837273
Ben Silverman <bensilverman10@gmail.com>
2021-09-16 15:31:34 +0100

    Convert post links to cards

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { styled } from "../stitches.config";
 
 import { Date } from "../components/Date";
 import { Layout } from "../components/Layout";
 
 import { getSortedPostsData } from "../lib/posts";
 import { Container } from "../components/Container";
-import { TextLink } from "../components/TextLink";
 import { GetStaticProps } from "next";
+import { H1 } from "../components/Heading";
+import { Text } from "../components/Text";
+import { CardLink } from "../components/Card";
+
+const PostsContainer = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+  gap: "$1",
+});
 
 interface Props {
   allPostsData: { slug: string; date: string; title: string }[];
@@ -15,18 +24,24 @@ interface Props {
 const PostsPage = ({ allPostsData }: Props): JSX.Element => {
   return (
     <Layout title="Posts">
+      <Container as="div">
+        <header>
+          <H1>Posts</H1>
+          <Text>
+            Here's a collection of posts I've written about different things! Probably (mostly) all
+            programming-related but who knows? Maybe some other topics will slip in here too.
+          </Text>
+        </header>
+      </Container>
       <Container id="main-content">
-        <ul>
-          {allPostsData.map(({ slug, date, title }) => (
-            <li key={slug}>
-              <TextLink to={`/posts/${slug}`}>{title}</TextLink>
-              <br />
-              {slug}
-              <br />
+        <PostsContainer>
+          {allPostsData.map(({ slug, date, title, description }) => (
+            <CardLink title={title} key={slug} to={`/posts/${slug}`}>
               <Date dateString={date} />
-            </li>
+              <Text>{description}</Text>
+            </CardLink>
           ))}
-        </ul>
+        </PostsContainer>
       </Container>
     </Layout>
   );

959e515f61a0ae628dd5ba087438ab93dd2dfb41
benslv <bensilverman10@gmail.com>
2023-01-04 22:25:14 +0000

    Create static first page

--- app/root.tsx
+++ app/root.tsx
@@ -1,32 +1,44 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
+	Links,
+	LiveReload,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
 } from "@remix-run/react";
 
+import styles from "./tailwind.css";
+
 export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
+	charset: "utf-8",
+	title: "New Remix App",
+	viewport: "width=device-width,initial-scale=1",
 });
 
+export const links: LinksFunction = () => {
+	return [
+		{
+			rel: "stylesheet",
+			href: "https://unpkg.com/modern-css-reset@1.4.0/dist/reset.min.css",
+		},
+		{ rel: "stylesheet", href: styles },
+	];
+};
+
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+				<Meta />
+				<Links />
+			</head>
+			<body className="mx-4 subpixel-antialiased text-white bg-gray-900 sm:max-w-2xl sm:mx-auto">
+				<Outlet />
+				<ScrollRestoration />
+				<Scripts />
+				<LiveReload />
+			</body>
+		</html>
+	);
 }
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,76 @@
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+	return (
+		<div className="flex flex-col space-y-8">
+			<section>
+				<h1 className="text-lg font-bold">Ben Silverman</h1>
+				<p>Software Developer @ Warner Bros. Discovery</p>
+			</section>
+			<section>
+				<h1 className="mb-2 uppercase text-emerald-300">About Me</h1>
+				<p>All things web dev. </p>
+				<p>
+					I love the creative process&mdash;designing, building,
+					refining&mdash;and strive to produce things that others want
+					to use. Currently working on{" "}
+					<a
+						href="https://lettuce.watch"
+						target="_blank"
+						rel="noopener noreferrer"
+						className="px-1 transition-colors rounded bg-opacity-20 text-emerald-300 hover:text-emerald-200 hover:bg-opacity-30 bg-emerald-500"
+					>
+						Lettucewatch.
+					</a>
+				</p>
+				<br />
+				<p>
+					You can find me on{" "}
+					<a
+						href="https://twitter.com/bensilverman_"
+						target="_blank"
+						rel="noopener noreferrer"
+						className="px-1 transition-colors rounded bg-opacity-20 text-emerald-300 hover:text-emerald-200 hover:bg-opacity-30 bg-emerald-500"
+					>
+						Twitter
+					</a>{" "}
+					and{" "}
+					<a
+						href="https://github.com/benslv/"
+						target="_blank"
+						rel="noopener noreferrer"
+						className="px-1 transition-colors rounded bg-opacity-20 text-emerald-300 hover:text-emerald-200 hover:bg-opacity-30 bg-emerald-500"
+					>
+						GitHub
+					</a>
+					, or even send me an email!
+				</p>
+			</section>
+			<section>
+				<h1 className="mb-2 uppercase text-emerald-300">Experience</h1>
+				<div className="flex flex-col -mx-4 overflow-hidden sm:rounded-lg">
+					<div className="flex flex-col px-4 py-2 bg-gray-800 sm:space-x-4 sm:items-center sm:flex-row">
+						<p className="text-sm italic sm:w-2/12">2022-present</p>
+						<div className="sm:w-max">
+							<p className="font-bold">Warner Bros. Discovery</p>
+							<p className="text-gray-200 ">
+								Developing highly-optimised frontend software
+								for connected devices.
+							</p>
+						</div>
+					</div>
+					<div className="flex flex-col px-4 py-2 bg-gray-800 sm:space-x-4 sm:items-center sm:flex-row">
+						<p className="text-sm italic sm:w-2/12">2020-2021</p>
+						<div className="sm:w-max">
+							<p className="font-bold">Arm Ltd.</p>
+							<p className="text-gray-200 ">
+								React SPA development & Azure policy management.
+							</p>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section>
+				<h1 className="mb-2 uppercase text-emerald-300">My Work</h1>
+			</section>
+		</div>
+	);
 }

34c0b0790b79aed621fa100e25985e9effc1b61c
benslv <bensilverman10@gmail.com>
2024-05-13 22:05:40 +0100

    begin

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
 import { Link } from "@remix-run/react";
+import { Github, Linkedin, Twitter } from "iconoir-react";
 
 export const meta: MetaFunction = () => {
 	return [
@@ -10,34 +11,62 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
 	return (
-		<div className="m-8 w-fit rounded-md border bg-white p-8">
-			<p className="mb-2">👋</p>
-			<h2 className="text-2xl font-medium text-zinc-800">Ben Silverman</h2>
-			<p>
-				Software Engineer at{" "}
-				<a
-					href="https://wbd.com"
-					target="_blank"
-					className="text-blue-500 underline decoration-blue-300 underline-offset-2">
-					Warner Bros. Discovery
-				</a>
-			</p>
-			<div className="mt-4 flex flex-col gap-y-2">
-				<Link
-					to="/about"
-					className="underline decoration-zinc-300 underline-offset-2">
-					about
-				</Link>
-				<Link
-					to="/posts"
-					className="underline decoration-zinc-300 underline-offset-2">
-					posts
-				</Link>
-				<Link
-					to="/projects"
-					className="underline decoration-zinc-300 underline-offset-2">
-					projects
-				</Link>
+		<div className="m-8 flex flex-col gap-4">
+			<div className="w-fit rounded-md border bg-white p-8">
+				<p className="mb-2">👋</p>
+				<h2 className="text-2xl font-medium text-zinc-800">Ben Silverman</h2>
+				<p>
+					Software Engineer at{" "}
+					<a
+						href="https://wbd.com"
+						target="_blank"
+						className="text-blue-500 underline decoration-blue-300 underline-offset-2">
+						Warner Bros. Discovery
+					</a>
+				</p>
+				<div className="mt-4 flex flex-col gap-y-2">
+					<Link
+						to="/about"
+						className="underline decoration-zinc-300 underline-offset-2">
+						about
+					</Link>
+					<Link
+						to="/posts"
+						className="underline decoration-zinc-300 underline-offset-2">
+						posts
+					</Link>
+					<Link
+						to="/projects"
+						className="underline decoration-zinc-300 underline-offset-2">
+						projects
+					</Link>
+				</div>
+			</div>
+			<div className="flex gap-x-4">
+				<div className="relative w-fit rounded-md bg-[#333333] p-4 transition-colors hover:bg-[#222222]">
+					<Link
+						to="https://github.com/benslv"
+						target="_blank"
+						className="before:absolute before:inset-0">
+						<Github color="#ffffff" />
+					</Link>
+				</div>
+				<div className="relative w-fit rounded-md bg-[#0a66c2] p-4 transition-colors hover:bg-[#0959aa]">
+					<Link
+						to="https://linkedin.com/in/ben-silverman"
+						target="_blank"
+						className="before:absolute before:inset-0">
+						<Linkedin color="#ffffff" />
+					</Link>
+				</div>
+				<div className="relative w-fit rounded-md bg-[#1da1f2] p-4 transition-colors hover:bg-[#0d8bd9]">
+					<Link
+						to="https://twitter.com/bensilverman_"
+						target="_blank"
+						className="before:absolute before:inset-0">
+						<Twitter color="#ffffff" />
+					</Link>
+				</div>
 			</div>
 		</div>
 	);

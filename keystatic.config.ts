31bf017375f8bf8fe157783bd55d33a74790c6db
Ben Silverman <>
2024-05-15 15:34:57 +0100

    move to github storage

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -2,7 +2,11 @@ import { collection, config, fields } from "@keystatic/core";
 
 export default config({
 	storage: {
-		kind: "local",
+		kind: "github",
+		repo: {
+			owner: "benslv",
+			name: "benslv.dev",
+		},
 	},
 	collections: {
 		posts: collection({

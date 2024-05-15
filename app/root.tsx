d90e6cc363fb7024abc1663c70cc340bfda54200
Ben Silverman <>
2024-05-15 09:26:44 +0100

    add favicon

--- app/root.tsx
+++ app/root.tsx
@@ -31,6 +31,10 @@ export const links: LinksFunction = () => [
 		rel: "stylesheet",
 		href: "https://fonts.googleapis.com/css2?family=Kalam:wght@400&display=swap",
 	},
+	{
+		rel: "icon",
+		href: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👋</text></svg>",
+	},
 ];
 
 export function Layout({ children }: { children: ReactNode }) {

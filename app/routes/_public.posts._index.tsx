7033ecc2a4257d780ddacd7d6b3f5b675e9b9e89
Ben Silverman <>
2024-05-18 13:09:29 +0100

    improve post sorting

--- app/routes/_public.posts._index.tsx
+++ app/routes/_public.posts._index.tsx
@@ -16,7 +16,16 @@ export async function loader() {
 	// 2. Read the "Posts" collection
 	const posts = await reader.collections.posts.all();
 
-	return json({ posts });
+	const publishedPosts = posts
+		.filter((post) => post.entry.published || process.env.SHOW_DRAFT_POSTS)
+		.sort((a, b) => {
+			return (
+				new Date(b.entry.publishedDate!).getTime() -
+				new Date(a.entry.publishedDate!).getTime()
+			);
+		});
+
+	return json({ posts: publishedPosts });
 }
 
 export default function Page() {
@@ -27,19 +36,17 @@ export default function Page() {
 			<h1 className="mb-4 font-handwriting text-3xl text-zinc-800">Posts 📝</h1>
 			<ul className="space-y-2">
 				{posts.map((post) => (
-					<li key={post.slug}>
+					<li key={post.slug} className="flex justify-between gap-x-2">
 						<Link
 							to={`/posts/${post.slug}`}
-							className="flex justify-between gap-x-2">
-							<span className="text-zinc-800 underline decoration-zinc-300 decoration-2 underline-offset-2">
-								{post.entry.title}
-							</span>
-							<span className="text-nowrap tabular-nums text-zinc-400">
-								{new Date(post.entry.publishedDate!).toLocaleString("en-GB", {
-									dateStyle: "medium",
-								})}
-							</span>
+							className="text-zinc-800 underline decoration-zinc-300 decoration-2 underline-offset-2">
+							<span className="">{post.entry.title}</span>
 						</Link>
+						<span className="text-nowrap tabular-nums text-zinc-400">
+							{new Date(post.entry.publishedDate!).toLocaleString("en-GB", {
+								dateStyle: "medium",
+							})}
+						</span>
 					</li>
 				))}
 			</ul>

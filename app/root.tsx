75e89845e3c52f00a730021f373f866a0d6f1749
Ben Silverman <bensilverman10@gmail.com>
2024-09-08 18:05:14 +0100

    remove font load

--- app/root.tsx
+++ app/root.tsx
@@ -24,10 +24,6 @@ export const links: LinksFunction = () => [
 		href: "https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500&display=swap",
 	},
 	{
-		rel: "stylesheet",
-		href: "https://fonts.googleapis.com/css2?family=Kalam:wght@400&display=swap",
-	},
-	{
 		rel: "icon",
 		href: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👋</text></svg>",
 	},

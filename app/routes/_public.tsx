956380ec5da064c27614c14c264c1997eb56917d
Ben Silverman <bensilverman10@gmail.com>
2024-09-11 21:31:51 +0100

    new navbar

--- app/routes/_public.tsx
+++ app/routes/_public.tsx
@@ -15,30 +15,28 @@ export default function PublicLayout() {
 
 	return (
 		<div className="flex h-full flex-col">
-			<header className="w-full border-b border-gray-200 bg-gray-50 p-4">
-				<div className="mx-auto flex max-w-screen-sm ">
-					<nav>
-						<ul className="flex gap-x-4 text-sm font-medium text-zinc-400">
-							<li>
-								<Link to="/" className="transition-colors hover:text-zinc-500">
-									Home
-								</Link>
-							</li>
-							<li>
-								<Link
-									to="/posts"
-									className="transition-colors hover:text-zinc-500">
-									Posts
-								</Link>
-							</li>
-						</ul>
-					</nav>
-				</div>
+			<header className="w-full bg-white px-4">
+				<nav className="mx-auto mt-4 flex max-w-screen-sm rounded-full border border-zinc-200 bg-gradient-to-b from-zinc-50 to-zinc-100 px-4 py-2 text-zinc-400 shadow-inner">
+					<ul className="flex gap-x-4 text-sm font-medium">
+						<li>
+							<Link to="/" className="transition-colors hover:text-zinc-500">
+								Home
+							</Link>
+						</li>
+						<li>
+							<Link
+								to="/posts"
+								className="transition-colors hover:text-zinc-500">
+								Posts
+							</Link>
+						</li>
+					</ul>
+				</nav>
 			</header>
-			<div className="w-full flex-1 bg-white py-8">
+			<div className="w-full flex-1 bg-white px-4 py-8">
 				<Outlet />
 			</div>
-			<footer className="w-full border-t border-gray-200 bg-gray-50 p-4">
+			<footer className="w-full border-t border-zinc-200 bg-zinc-50 p-4 shadow-inner">
 				<div className="mx-auto flex max-w-screen-sm justify-between">
 					<NowPlaying currentTrackPromise={currentTrackPromise} />
 					<SocialIcons />
